1987598a83d629153abb9ba7a51031116e220ca3
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-09-02 19:00:55 +0300

    added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,28 @@
 $(function(){
   
+
+  $('.technologies__star').rateYo({
+    starWidth: "24px",
+    ratedFill: "#06B25F",
+    readOnly: true,
+  });
+
+
+
+
+  $('.about__items').slick({
+    arrows: false,
+    dots:true,
+  })
+
+
+
+  $('.header__link-service').on('click' , function () {
+    $('.header__services').toggleClass('header__services--active')
+    $('.header__link-service').toggleClass('header__link-service--active')
+  })
+
+
+
+
 });
