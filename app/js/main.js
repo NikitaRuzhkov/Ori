8466832f4730d331fee28b96e88abef938380464
NikitaRuzhkov <nruzshkov@karazin.ua>
2023-09-12 16:48:35 +0300

    added about porfolio services technologies

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-  
+$(function () {
+
 
   $('.technologies__star').rateYo({
     starWidth: "24px",
@@ -12,15 +12,30 @@ $(function(){
 
   $('.about__items').slick({
     arrows: false,
-    dots:true,
-  })
+    dots: true,
+  });
 
+  $('.vr__projects-items').slick({
+    slidesToShow: 3,
+  });
 
+  $('.faq__title').on('click', function () {
+    $(this).siblings().slideToggle();    
+    $(this).toggleClass('faq__text--active')
+  });
 
-  $('.header__link-service').on('click' , function () {
+  $('.header__link-service').on('click', function () {
     $('.header__services').toggleClass('header__services--active')
     $('.header__link-service').toggleClass('header__link-service--active')
-  })
+  });
+  $('.header__link-technologies').on('click', function () {
+    $('.header__technologies').toggleClass('header__technologies--active')
+    $('.header__link-technologies').toggleClass('header__link-technologies--active')
+  });
+  $('.header__link-about').on('click', function () {
+    $('.header__about').toggleClass('header__about--active')
+    $('.header__link-about').toggleClass('header__link-about--active')
+  });
 
 
 
